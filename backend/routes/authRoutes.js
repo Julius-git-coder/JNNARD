ef160ed10a7bb7da0cf37e991c5c5fec95cea540
Julius <daganajulius5@gmail.com>
2026-01-21 22:49:41 +0000

    update

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login, verifyEmail, forgotPassword, resetPassword, updateProfile } from '../controllers/authController.js';
+import { register, login, verifyEmail, forgotPassword, resetPassword, updateProfile, validateOTP } from '../controllers/authController.js';
 import { parser } from '../utils/cloudinary.js';
 import { protect } from '../middleware/authMiddleware.js';
 
@@ -10,6 +10,7 @@ router.post('/login', login);
 router.post('/verify-email', verifyEmail);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
+router.post('/validate-otp', validateOTP);
 router.put('/profile', protect, parser.single('avatar'), updateProfile);
 
 export default router;
